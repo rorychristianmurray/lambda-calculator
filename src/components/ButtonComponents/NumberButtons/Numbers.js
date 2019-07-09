26d2c7f9345beed6ce1ded5e5e127a30bebce9fe
Rory Murray <rory.christian.murray@gmail.com>
2019-07-09 19:14:07 -0400

    scaffolding for numbers

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,26 @@
 import React from "react";
+import NumberButton from "./NumberButton";
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+import { numbers } from "../../../data";
+
+export const Numbers = () => {
   // STEP 2 - add the imported data to state
+
+  const [numbersList, setNumber] = useState(numbers);
+
+  console.log("Numbers this.state", this.state);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component */}
+      {numbersList.map(number => {
+        return <NumberButton number={number} setNumber={setNumber} />;
+      })}
     </div>
   );
 };
